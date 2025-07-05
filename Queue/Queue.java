f1852b2d1ee45ca92de4aa7d102d20dc76853902
AshithGannarapu16 <ashithgannarapu@gmail.com>
2025-07-05 22:05:37 +0530

    circular Queue

---
+++ Queue/Queue.java
@@ -0,0 +1,44 @@
+public class Circular {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node head, int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            newNode.next = newNode; // Point to itself
+            return newNode;
+        }
+        Node temp = head;
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+        temp.next = newNode; // Link the last node to the new node
+        newNode.next = head; // New node points to head
+        return head;
+    }
+
+    public static void display(Node head) {
+        if (head == null) return;
+        Node temp = head;
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        int values[] = {1, 2, 3, 4, 5};
+        for (int value : values) {
+            head = insert(head, value);
+        }
+        display(head);
+    }
+}
