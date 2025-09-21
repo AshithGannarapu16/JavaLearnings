4e74f5b897f79f50c9cf134adb71dcb3e26c5621
AshithGannarapu16 <ashithgannarapu@gmail.com>
2025-09-21 23:46:37 +0530

    scanner

--- print/dataType.java
+++ print/dataType.java
@@ -1,7 +1,12 @@
 public class dataType{
     public static void main(String[] args){
-        long a=10000;
+        // long a=10000;
+        // int b=(int)a;
+        // System.out.println(b);
+
+        long a=10000000000l;
         int b=(int)a;
-        System.out.println(b);
+        System.out.print(b);
+
     }
 }
---
+++ print/sc.java
@@ -0,0 +1,17 @@
+
+import java.awt.event.FocusAdapter;
+import java.lang.foreign.SymbolLookup;
+import java.util.Scanner;
+
+public class sc{
+    public static void main(String[] args) {
+        
+        Scanner Sc=new Scanner(System.in);
+        int x=Sc.nextInt();
+        System.out.println(x);
+
+        double a=17.67;
+        float b=(float) a;
+        System.out.println(b);
+    }
+}
