df279e36035ea34deca2cc62d16c0c7e7c388046
AshithGannarapu16 <ashithgannarapu@gmail.com>
2025-09-22 22:56:34 +0530

    print

---
+++ print/assig.java
@@ -0,0 +1,28 @@
+
+import java.awt.Polygon;
+import java.net.SocketImpl;
+
+public  class assig{
+    public static void main(String[] args) {
+        int a=10;
+        a += 20;
+         // a=a+b
+        System.out.println(a);
+
+        int b=10;
+        b -= 1;
+        System.out.println(b);
+
+        int c=10;
+        c += 50;
+        System.out.println(c);
+
+        c -= 5;
+        System.out.println(c);
+
+        int x=10;
+        int y=x++ + x++;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
