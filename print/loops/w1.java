f6c7ffacbeba1de0aa14864a881ee0c01d4b61ca
AshithGannarapu16 <ashithgannarapu@gmail.com>
2025-09-24 16:35:07 +0530

    changes

---
+++ print/loops/w1.java
@@ -0,0 +1,15 @@
+public class w1{
+    public static void main(String[] args) {
+        // int n=3;
+        // while(n<=10){
+        //     System.out.println(n);
+        //     n=n*3;
+        // }
+
+        int i=1;
+        while(i>=5){
+            System.out.println(i);
+            i= i+1;
+        }
+    }
+}
