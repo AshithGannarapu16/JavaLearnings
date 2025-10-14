3e201e6af4b2696964617b4c600f1fb97e925666
AshithGannarapu16 <ashithgannarapu@gmail.com>
2025-10-14 16:50:24 +0530

    changes

---
+++ Trees/Searching.java
@@ -0,0 +1,26 @@
+public class Searching {
+
+
+    static int binarySearch(int []a, int key){
+        int l=0, h=a.length-1, mid=0;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(key==a[mid]){
+                return mid;
+            }
+            else if(key<a[mid]){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int [] a={3,5,6,8,12,15,16,19,21};
+        int key=13;
+        System.out.println(binarySearch(a,key));
+    }
+}
