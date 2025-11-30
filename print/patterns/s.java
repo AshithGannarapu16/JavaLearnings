488b58145dfd1c3d1eed917592e543a9bf595807
AshithGannarapu16 <ashithgannarapu@gmail.com>
2025-11-30 20:05:57 +0530

    patterns

---
+++ print/patterns/s.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class s{
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<=n;i++){
+        System.out.print("*");
+        for(int j=0;j<=n-1;j++){
+            System.out.print("_");
+        }
+        System.out.println("*");
+       } 
+    }
+}
